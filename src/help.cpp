9595a11799366b9c09d2e9e7a6ef24eab75190b0
Shivang Srivastava <satyamsri8@users.noreply.github.com>
2017-11-13 13:06:59 +0530

    Updating Quit Command

--- src/help.cpp
+++ src/help.cpp
@@ -9,6 +9,6 @@ void help()
 	cout<<"2. run	or r : \n\tRun the program until it ends or breakpoint is enountered."<<endl<<endl;
 	cout<<"3. step or s : \n\tIt will run the program one instruction at a time."<<endl<<endl;
 	cout<<"4. print or p 'Resister / Memory Address'	: \n\tIt prints the value of register or Memory location. \n\tfor ex p A will print the value of register A. \n\tp x2500 will print the value at memory location x2500 if any."<<endl<<endl;
-	cout<<"5. quit or q 'line number' : \n\tQuits the debugger."<<endl<<endl;
+	cout<<"5. quit or q : \n\tQuits the debugger."<<endl<<endl;
 	cout<<"6. help : \n\tIt will show all commands of debugger."<<endl<<endl;
 }
